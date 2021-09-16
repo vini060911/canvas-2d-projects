4ef6a49f8234a5ee3c8654cc52bd86b5e3846e21
vini060911 <90809396+vini060911@users.noreply.github.com>
2021-09-16 18:05:20 -0300

    Update novo.java

--- novo.java
+++ novo.java
@@ -1,25 +1,3 @@
-public class x {}
-"cvmfdbnvrgbhrfbhsihgvbdivbgfdivbfigvhfbvjhfdbvidhybvg
-  fbvfdabfdbadf
-  gg fgb
-  
-  eh
-  trh
-  rt
-  hw
-  rtj
-  tb
-  g
-  bte
-  he
-  tajht
-  juy
-  liullkuiç
-  ygj
-  tgnjsd
-  yjy
-  k
-  hnt
-  rkj
+
   sr
   
